521542fc7599d2c8d863187f2adaacb5e4995546
태고밍 <68893329+TaeKyeong-coder@users.noreply.github.com>
2023-03-20 13:24:45 +0900

    이미지 변경
    이미지 변경.. 좀 더 고화질로

--- app/src/main/java/tk/clone/instagram/data/StoryDummyData.kt
+++ app/src/main/java/tk/clone/instagram/data/StoryDummyData.kt
@@ -12,14 +12,14 @@ object StoryDummyData {
         12,
         15,
         "Twitter for web",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOJh-XwQ_jvK7nQ_38BgAbHcWOPt24YNCaMvYN5CWG04AX4-lig1O8mjnJ8OsDwdWFvAg&usqp=CAU"
+        "https://cdnweb01.wikitree.co.kr/webdata/editor/202302/07/img_20230207142115_3e5799ed.webp"
     )
     val storyList = listOf(
         originStory,
         originStory.copy(
             id = 2, author = "ming_chu85", handle = "@ming_chu85",
             authorImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg7CvT_Z8DsZYjgUn08B5duYhsJRUllhlvSw&usqp=CAU",
-            storyImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlryBnMRle_5jHRxyTOpOFmXRgezPHh6lS3w&usqp=CAU",
+            storyImageUrl = "https://i.ytimg.com/vi/8kfcakCUY4Q/maxresdefault.jpg",
             time = "11m"
         ),
         originStory.copy(
